9f03c979c2bd99f7e6309bec115ccf69f47be988
MR.G4NS <70622035+soniwiputra@users.noreply.github.com>
2021-02-22 20:26:32 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,17 @@
+const donasi = () => { 
+	return `       
+┏ *〈 APBOT 〉*
+╿
+┯┷ *〈 DONASI NYA OMM 〉*
+┠──────────────╼
+┠≽ *〈 OVO 〉*
+┃  *0852-2098-0276*
+┠──────────────╼
+┠≽ *〈 PULSA 〉*
+┃  *0852-2098-0276* 
+┠──────────────╼
+┷┯.
+ ╰╼≽ *Developer © APBOT MR.G4S`
+}
+
+exports.donasi = donasi
